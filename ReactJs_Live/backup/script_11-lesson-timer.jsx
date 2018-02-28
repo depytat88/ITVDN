0f1ff562dccb82f96879bcfeedb06116539f0887
d.zyabko <d.zyabko@itransition.com>
2018-02-28 14:48:22 +0300

    lesson 11

---
+++ ReactJs_Live/backup/script_11-lesson-timer.jsx
@@ -0,0 +1,80 @@
+class TimerWrapper extends React.Component {
+    constructor (props) {
+        super (props);
+        this.startTimer = this.startTimer.bind(this);
+        this.state = {
+            timeLeft: null,
+            timer: null
+        }
+    }
+
+    startTimer (timeLeft) {
+        clearInterval(this.state.timer);
+        let timer = setInterval(()=>{
+            timeLeft = this.state.timeLeft - 1;
+            if (timeLeft === 0) {
+                clearInterval(timer);
+            }
+            this.setState({
+                timeLeft: timeLeft
+            });
+        }, 1000)
+
+        return (
+            this.setState({
+                timeLeft: timeLeft,
+                timer: timer
+            })
+        );
+    }
+
+    render () {
+        return (
+            <div>
+                <h2>Timer</h2>
+                <div>
+                    <Button time="5" startTimer={this.startTimer}/>
+                    <Button time="10" startTimer={this.startTimer}/>
+                    <Button time="15" startTimer={this.startTimer}/>
+                    <TimerDisplay timeLeft={this.state.timeLeft}/>
+                    <audio id="end" preload="auto" src="media/beatlesmail.wav"></audio>
+                </div>
+            </div>
+        );
+    }
+}
+
+class Button extends React.Component {
+    handleStartTimer () {
+        return (
+            this.props.startTimer(this.props.time)
+        );
+    }
+
+    render () {
+        return (
+            <button onClick={this.handleStartTimer.bind(this)}>{this.props.time} секунд</button>
+        );
+    }
+}
+
+class TimerDisplay extends React.Component {
+    render () {
+        if (this.props.timeLeft === 0) {
+            document.getElementById("end").play()
+        }
+        if (this.props.timeLeft === 0 || this.props.timeLeft === null) {
+            return (
+                <div></div>
+            );
+        }
+        return (
+            <h1>Осталось времени {this.props.timeLeft}</h1>
+        );
+    }
+}
+
+ReactDOM.render(
+    <TimerWrapper />, 
+    document.getElementById('content')
+);
